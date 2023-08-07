bc2286d1ade4d7454001e7263415211991eddc34
Marcelo Pezzolano <mpezzolano@gmail.com>
2023-08-07 20:29:45 -0300

    small refactor

--- tasks/tasks.go
+++ tasks/tasks.go
@@ -3,11 +3,45 @@ package tasks
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/rs/xid"
 	"go.uber.org/zap"
 )
+
+type TaskError struct {
+	ID    string
+	Task  string
+	Cause error
+}
+
+func (e *TaskError) Error() string {
+	return fmt.Sprintf("task %s (ID %s) failed: %v", e.Task, e.ID, e.Cause)
+}
+
+type Task struct {
+	sync.Mutex
+	id            string
+	Name          string
+	Interval      time.Duration
+	RunOnce       bool
+	StartAfter    time.Time
+	TaskFunc      func() error
+	OnFail        func(error)
+	timer         *time.Timer
+	ctx           context.Context
+	cancel        context.CancelFunc
+	ExecutionTime time.Duration
+	LastRun       time.Time
+	SuccessCount  int
+	FailureCount  int
+}
+
+type Scheduler struct {
+	sync.RWMutex
+	tasks map[string]*Task
+}
 
 var logger, _ = zap.NewProduction()
 
